b3b4de70a313f0d10f86f98f69c91e88aec42dbb
Thokozile Soko <thokozile.soko@yahoo.com>
2021-02-13 13:26:09 -0500

    Ran npm init, created a server.js file and created sql files in db.

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Garden Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", true);
