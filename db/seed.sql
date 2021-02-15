f6c399f74931c4c802c06246c218be0c4d74077e
Thokozile Soko <thokozile.soko@yahoo.com>
2021-02-14 20:30:16 -0500

    I added a new block handlebar so that the burgers that have already been devoured can only be deleted. I also hard coded that the burger created is not devoured. Trying to get my buttons to work is the hard part. Not sure why they aren't working.

--- db/seed.sql
+++ db/seed.sql
@@ -4,3 +4,4 @@ USE burgers_db;
 INSERT INTO burgers (burger_name, devoured) VALUES ("Garden Burger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Salmon Burger", false);
